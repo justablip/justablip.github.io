4fff46795ff001ae0a01952154462fc9931652e5
antoniocosta <abmc@Snowdens-MacBook-Air.local>
2021-04-29 13:26:33 +0200

    Updated feed

--- mixcloud2podcast/rss.sh
+++ mixcloud2podcast/rss.sh
@@ -34,7 +34,7 @@ RSS_DATE=$(date -R) # now
 
 header () {
 echo """<?xml version='1.0' encoding='UTF-8' ?>
-<rss version='2.0' xmlns:atom='http://www.w3.org/2005/Atom' xmlns:itunes='http://www.itunes.com/dtds/podcast-1.0.dtd'>
+<rss xmlns:atom='http://www.w3.org/2005/Atom' xmlns:content='http://purl.org/rss/1.0/modules/content/' xmlns:googleplay='http://www.google.com/schemas/play-podcasts/1.0' xmlns:itunes='http://www.itunes.com/dtds/podcast-1.0.dtd' version='2.0'>
 """ > ./feedtop
 echo """<channel>
         <atom:link href='$RSS_LINK' rel='self' type='application/rss+xml' />
